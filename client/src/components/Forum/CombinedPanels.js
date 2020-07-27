918419eb4703bc6cb2ce787638909d01a2bdbf3a
Lucian Condrea <lucian@condrea.dev>
2020-07-27 14:26:55 -0400

    cleanup CombinedPanels, remove Paper

--- client/src/components/Forum/CombinedPanels.js
+++ client/src/components/Forum/CombinedPanels.js
@@ -1,62 +1,42 @@
 import React from "react";
-
-import { Grid, Paper, styled } from "@material-ui/core";
-
+import { Grid, styled } from "@material-ui/core";
 import LeftPanel from "./LeftPanel";
 import RightPanel from "./RightPanel";
 
-
 const MyGrid = styled(Grid)({
-  // container: true,
   padding: 20,
   margin: 0
 });
 
-
-
-//THis puts the right and left panels together into one component.
 function CombinedPanels(props) {
   return (
-    <div>
-      {/*comment below while working on postdetail.js */}
+    <MyGrid container>
+      <Grid item xs={6}>
+        <Grid container justify="center" spacing={1}>
+          <LeftPanel
+            userPosts={props.userPosts}
+            setUserPosts={props.setUserPosts}
+            addPost={props.addPost}
+          />
+        </Grid>
+      </Grid>
 
-      <Paper>
-        <MyGrid container>
-          <Grid item xs={6}>
-            <Grid container justify="center" spacing={1}>
-              <LeftPanel
-                // styles={styles}
-                userPosts={props.userPosts}
-                setUserPosts={props.setUserPosts}
-                addPost={props.addPost}
-
-                // handleSubmit={props.handleSubmit}
-                // handleChangeText={props.handleChangeText}
-                // handleChangeTitle = {props.handleChangeTitle}
-                // newPost={props.newPost}
-                // setNewPost={props.setNewPost}
-              />
-            </Grid>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Grid container justify="flex-start" spacing={1}>
-              <RightPanel
-                userPosts={props.userPosts}
-                // setUserPost={props.setUserPost}
-                // styles={styles}
-                deletePost={props.deletePost}
-                editing={props.editing}
-                setEditing={props.setEditing}
-                editPost={props.editPost}
-                currentPost={props.currentPost}
-                updatePost={props.updatePost}
-              />
-            </Grid>
-          </Grid>
-        </MyGrid>
-      </Paper>
-    </div>
+      <Grid item xs={6}>
+        <Grid container justify="flex-start" spacing={1}>
+          <RightPanel
+            userPosts={props.userPosts}
+            // setUserPost={props.setUserPost}
+            // styles={styles}
+            deletePost={props.deletePost}
+            editing={props.editing}
+            setEditing={props.setEditing}
+            editPost={props.editPost}
+            currentPost={props.currentPost}
+            updatePost={props.updatePost}
+          />
+        </Grid>
+      </Grid>
+    </MyGrid>
   );
 }
 
