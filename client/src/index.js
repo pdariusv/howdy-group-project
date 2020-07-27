b02f3e517dedff641cd099134457fc4ecfc29624
Lucian Condrea <lucian@condrea.dev>
2020-07-27 14:38:00 -0400

    clean index, remove unused theme provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-// import red from "@material-ui/core/colors/red";
-
-// import "./index.css";
 import App from "./App";
 
-const theme = createMuiTheme({
-  // palette: {
-  //   primary: red
-  // }  
-});
-
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
-    <Router>
-      <App />
-    </Router>
-  </MuiThemeProvider>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
