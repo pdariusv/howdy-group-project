1e430178bb5731c9e6f1701a0845926c0c28b185
Weltgeist <narcisse118@gmail.com>
2020-07-28 16:52:28 -0400

    added context content, added handle bar script template

--- client/src/HBhandler.js
+++ client/src/HBhandler.js
@@ -1,4 +1,12 @@
-
+const context = {
+  termOfUse : [
+    {
+      name:"ownership",
+      title:"Ownership of Site; Agreement to Terms of Use",
+      content: "These Terms and Conditions of Use (the \"Terms of Use\") apply to the Apple web site located at www.apple.com, and all associated sites linked to www.apple.com by Apple, its subsidiaries and affiliates, including Apple sites around the world (collectively, the \"Site\"). The Site is the property of Apple Inc. (\"Apple\") and its licensors. BY USING THE SITE, YOU AGREE TO THESE TERMS OF USE; IF YOU DO NOT AGREE, DO NOT USE THE SITE.Apple reserves the right, at its sole discretion, to change, modify, add or remove portions of these Terms of Use, at any time. It is your responsibility to check these Terms of Use periodically for changes. Your continued use of the Site following the posting of changes will mean that you accept and agree to the changes. As long as you comply with these Terms of Use, Apple grants you a personal, non-exclusive, non-transferable, limited privilege to enter and use the Site."
+    }
+  ]
+}
 
 
 
