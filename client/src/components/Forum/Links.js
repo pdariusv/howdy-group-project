938bc80623805db55689a73f189015a5f0558141
Luis Alerto Zamora <lazamorasanchez@investpsp.ca>
2020-07-28 18:36:02 -0400

    Code cleanup

--- client/src/components/Forum/Links.js
+++ client/src/components/Forum/Links.js
@@ -1,63 +1,17 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Box,  styled, Link, Typography } from "@material-ui/core";
 import {Link as RouterLink} from "react-router-dom"
-import StaticPage  from './StaticPage';
 
-//would be good to know what this stuff does.
-// const useStyles = makeStyles(theme => ({
-//   heading: { display: "flex", marginLeft: theme.spacing(2) },
-//   root: {
-//     "& > * + *": {
-//       marginTop: theme.spacing(2),
-//       marginLeft: theme.spacing(2),
-//       display: "flex"
-//     }
-//   }
-// }));
-
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     marginTop: theme.spacing(2),
-//     marginLeft: theme.spacing(2),
-//     display: "wrap",
-//     alignItems: 'flex-start'
-//   }
-// }));
-
-
-//this component is on the left side and is all the links for the guideline page and notification form which should be changed at some point.
 const MyBox = styled(Box)({
-  // container: true,
-
   paddingLeft: 25
 });
 
-// const MyBox = styled(Box)({
-//
-//   root: {
-//     "& Typography": {
-//       paddingLeft: 25
-//     }
-//   }
-// });
-
-// notifications_none
 export default function Links() {
-  // const classes = useStyles();
   const preventDefault = event => event.preventDefault();
 
   return (
-    <MyBox
-    // className={classes.root}
-    >
-    
-      <Typography
-        // className={classes.heading}
-        variant="subtitle1"
-        gutterBottom
-      >
+    <MyBox>    
+      <Typography variant="subtitle1" gutterBottom>
         Links
       </Typography>
       <div>
@@ -70,12 +24,12 @@ export default function Links() {
       </Link>
       <div>
       <Link variant="body2" href="#" onClick={preventDefault}>
-        Directives
+         Directives
       </Link>
       </div>
       <div>
         <Link component={RouterLink} to="/static-page">
-          Staic page using using react-router-dom TEST
+          Static Page
         </Link>
       </div>
     </MyBox>
--- client/src/components/Forum/StaticPage.js
+++ client/src/components/Forum/StaticPage.js
@@ -1,33 +1,16 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import { Box,  styled, Link, Typography } from "@material-ui/core";
-//import {StaticPage} from "react-router-dom"
+import { Box,  styled } from "@material-ui/core";
 
-import { Route } from "react-router-dom";
-
-
-
-//this component is on the left side and is all the links for the guideline page and notification form which should be changed at some point.
 const MyBox = styled(Box)({
-  // container: true,
-
   paddingLeft: 25
 });
 
-
-
-// notifications_none
 export default function StaticPage() {
-  
   return (
-    <MyBox
-    // className={classes.root}
-    >
+    <MyBox>
     <div>
-        This is a static page. SPA asterix
+        This is a static page
     </div>
-     </MyBox>
-    
+     </MyBox>    
   );
 }
