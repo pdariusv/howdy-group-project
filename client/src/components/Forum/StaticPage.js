ba475178aa831b64ce396d0ff9f636a54d6bf53a
Samuel Narcisse <narcisse118@gmail.com>
2020-08-18 17:59:12 -0400

    added padding to paper component

--- client/src/components/Forum/StaticPage.js
+++ client/src/components/Forum/StaticPage.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { Box, Container, Typography, styled } from "@material-ui/core";
+import { Box, Container, Typography, Paper, styled } from "@material-ui/core";
 import termOfUse from '../../data/term-of-use-data'
 
 const MyBox = styled(Box)({
   paddingLeft: 25
 });
 
+const MyContainer = styled(Container)({
+  marginBottom: 10 ,
+
+
+});
+
+const MyPaper = styled(Paper)({
+  paddingTop: 10,
+  paddingBottom: 10,
+  paddingRight: 10,
+  paddingLeft: 10 
+});
+
 const MySubSection = (props) => {
     return (<article id={props.nameSub}>
-      <Container className="container">
-        <Typography component="h2" color="textPrimary">{props.title}</Typography>
-        <Typography component="p" color="textSecondary">{props.content}</Typography>
-      </Container>
+      <MyContainer className="container">
+        <MyPaper elevation={2}>
+          <Typography component="h2" color="textPrimary">{props.title}</Typography>
+          <Typography component="p" color="textSecondary">{props.content}</Typography>
+        </MyPaper>
+      </MyContainer>
     </article>);
 }
 
